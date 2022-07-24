69326069fc61760e4834ff150a457dfab7add9cc
Deluxe Duck <deluxe.duck@yahoo.com>
2022-07-24 12:04:26 +0200

    deploy

---
+++ assets/script/classes.js
@@ -0,0 +1,75 @@
+class Project {
+    constructor(root, id, url, name, tech) {
+        this.root = root;
+        this.id = id;
+        this.url = url;
+
+        this.name = name
+        this.tech = tech
+
+        this.create()
+    }
+
+    create = () => {
+        let project = document.createElement('div');
+        project.id = "project-" + this.id;
+        project.className = "project";
+
+        let imageHolder = document.createElement('div')
+        imageHolder.className = "image-holder";
+
+        let link = document.createElement('a')
+        let button = document.createElement('button')
+        button.id = "project-" + this.id + "-btn"
+        button.className = "btn-3 hidden"
+        button.innerHTML = "launch project >"
+
+        let image = document.createElement('img')
+        image.id = "project-" + this.id + "-img"
+        image.className = "project-image"
+        // image.style.background = `url(${this.url})`        
+        imageHolder.onmouseover = () => {
+            image.classList.add('darken')
+            button.classList.remove('hidden')
+        }
+        imageHolder.onmouseout = () => {
+            button.classList.add('hidden')
+            image.classList.remove('darken')
+        }
+
+        link.href = "#"; link.append(button)
+        imageHolder.append(image, link);
+        project.append(imageHolder)
+
+        for (let x = 0; x < 3; x++) {
+            let p = document.createElement('p')
+            p.classList.add('bold')
+
+            switch (x) {
+                case 0:
+                    p.classList.add('featured')
+                    p.innerHTML = 'featured'
+                    break;
+                case 1:
+                    p.classList.add('project-name')
+                    p.id = 'project-' + this.id + '-name'
+                    p.innerHTML = this.name
+                    break;
+                case 2:
+                    p.classList.add('technology')
+                    p.id = 'project-' + this.id + '-tech'
+                    p.innerHTML = this.tech
+                    break;
+            }
+
+            project.append(p)
+
+        }
+
+        this.root.append(project)
+        console.log('created')
+
+    }
+
+}
+
---
+++ assets/script/script.js
@@ -0,0 +1,31 @@
+const projectList = document.querySelector('.projects-list')
+console.log(projectList)
+
+for (let i = 1; i <= 7; i++) {
+    let project = new Project(projectList, i, 'assets/img/project.jpg', 'projekt', 'Java Script')
+}
+
+function reveal() {
+    let queue = document.querySelectorAll('.reveal')
+
+    for (let i = 0; i < queue.length; i++) {
+
+        let elemTopDistance = queue[i].getBoundingClientRect().top;
+        let revealRange = 150;
+
+        console.log(elemTopDistance, window.innerHeight - revealRange)
+
+        if (elemTopDistance < window.innerHeight - revealRange) {
+            queue[i].classList.add('active')
+        } else {
+            queue[i].classList.remove('active')
+        }
+    }
+}
+
+window.addEventListener('scroll', reveal);
+window.onload = () => {
+    document.querySelector('#name')?.classList.add('move')
+    document.querySelector('.hero-content-illustration')?.classList.add('illustration-move')
+    document.querySelector('.hero-content')?.classList.add('revealup')
+}
